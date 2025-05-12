9f0aafecd5a5aae7e72f9e610c0cf1d2a7193536
Matt Hill <mattnine@protonmail.com>
2025-05-12 10:41:19 -0600

    add request to actione exection

--- startos/actions/nameToLogs.ts
+++ startos/actions/nameToLogs.ts
@@ -1,6 +1,7 @@
 import { sdk } from '../sdk'
 import { yamlFile } from '../file-models/config.yml'
 import { store } from '../file-models/store.json'
+import { setName } from './setName'
 
 export const nameToLogs = sdk.Action.withoutInput(
   // id
@@ -24,6 +25,8 @@ export const nameToLogs = sdk.Action.withoutInput(
   async ({ effects }) => {
     const name = (await yamlFile.read().const(effects))!.name
     console.info(`Hello ${name}`)
+
+    await sdk.action.requestOwn(effects, setName, 'important')
 
     return {
       version: '1',
