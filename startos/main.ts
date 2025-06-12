ba1ccf40510fc51a1cd0ef9ded28c99c7cbb8100
Matt Hill <mattnine@protonmail.com>
2025-06-11 20:22:26 -0600

    beta 28

--- startos/main.ts
+++ startos/main.ts
@@ -11,43 +11,33 @@ export const main = sdk.setupMain(async ({ effects, started }) => {
   console.info('Starting Hello World!')
 
   /**
-   * ======================== Additional Health Checks (optional) ========================
-   *
-   * In this section, we define *additional* health checks beyond those included with each daemon (below).
-   */
-  const additionalChecks: T.HealthCheck[] = []
-
-  /**
    * ======================== Daemons ========================
    *
    * In this section, we create one or more daemons that define the service runtime.
    *
    * Each daemon defines its own health check, which can optionally be exposed to the user.
    */
-  return sdk.Daemons.of(effects, started, additionalChecks).addDaemon(
-    'primary',
-    {
-      subcontainer: await sdk.SubContainer.of(
-        effects,
-        { imageId: 'hello-world' },
-        sdk.Mounts.of().mountVolume({
-          volumeId: 'main',
-          subpath: null,
-          mountpoint: '/data',
-          readonly: false,
+  return sdk.Daemons.of(effects, started).addDaemon('primary', {
+    subcontainer: await sdk.SubContainer.of(
+      effects,
+      { imageId: 'hello-world' },
+      sdk.Mounts.of().mountVolume({
+        volumeId: 'main',
+        subpath: null,
+        mountpoint: '/data',
+        readonly: false,
+      }),
+      'hello-world-sub',
+    ),
+    exec: { command: ['hello-world'] },
+    ready: {
+      display: 'Web Interface',
+      fn: () =>
+        sdk.healthCheck.checkPortListening(effects, uiPort, {
+          successMessage: 'The web interface is ready',
+          errorMessage: 'The web interface is not ready',
         }),
-        'hello-world-sub',
-      ),
-      exec: { command: ['hello-world'] },
-      ready: {
-        display: 'Web Interface',
-        fn: () =>
-          sdk.healthCheck.checkPortListening(effects, uiPort, {
-            successMessage: 'The web interface is ready',
-            errorMessage: 'The web interface is not ready',
-          }),
-      },
-      requires: [],
     },
-  )
+    requires: [],
+  })
 })
